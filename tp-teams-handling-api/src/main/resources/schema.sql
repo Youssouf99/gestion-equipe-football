1f1cd680b4183296d6ca406d1e8e8b3f911fbd2c
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-05-30 14:59:33 +0200

    feat(DATABASE) : add Schema.sql and data.sql

---
+++ tp-teams-handling-api/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS team (
+                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                    name VARCHAR(200),
+                                    slogan VARCHAR(500)
+);
